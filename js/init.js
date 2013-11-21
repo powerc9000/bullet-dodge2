11e829a9df772936417b93abeab214a8e6d3b7e7
powerc9000 <clay.murray8@gmail.com>
2013-11-20 19:46:57 -0700

    changed the way jetpacks works
    added in new dude sprites. Jetpack now only refuels when you hit zero or are on the ground. Added in an image when fuel is gone. Added in a sitting image when vx == 0

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-define(["head-on", "player", "ship", "keys", "bullets"],function($h, player, ship, keys, bullets){
+define(["head-on", "player", "ship", "keys", "bullets", "buttons"],function($h, player, ship, keys, bullets, buttons){
 	window.addEventListener("blur", function(){
 		if($h.game.started){
 			$h.pause();
@@ -20,47 +20,7 @@ define(["head-on", "player", "ship", "keys", "bullets"],function($h, player, shi
 		$h.canvas("hud").append("body");
 		// $h.canvas("background").append("body");
 		// $h.canvas("background").cavanas.style.position ="aboslute";
-		$h.startGameButton = $h.entity({
-			width: 200,
-			height: 100,
-			angle:0,
-			text: "Start Game",
-			position: $h.Vector($h.map.width/2 - 100, $h.map.height/2 - 50),
-			render: function(canvas){
-				if(!$h.game.started && !$h.game.gameOver && !$h.game.starting){
-					canvas.drawRect(this.width, this.height, this.position.x, this.position.y, "blue");
-					canvas.drawText("Start Game", this.position.x + this.width/2, this.position.y + this.height/2, "20px", "white", "center", "middle");
-				}
-				if(!$h.game.started && $h.game.gameOver && !$h.game.starting){
-					canvas.drawRect(this.width, this.height, this.position.x, this.position.y, "blue");
-					canvas.drawText("Retry", this.position.x + this.width/2, this.position.y + this.height/2, "20px", "white", "center", "middle");
-				}
-			},
-			click: function(){
-				if(!$h.game.gameOver && !$h.game.started && !$h.game.starting){
-					$h.game.starting = true;
-					$h.game.startTimeLeft = 5000;
-				}
-				if($h.game.gameOver && !$h.game.started && !$h.game.starting){
-					$h.events.trigger("reset");
-				}
-			},
-			init: function(){
-				var that = this;
-				$h.events.listen("click", function(mouse){
-					console.log(mouse)
-					var mouse = {
-						width:2,
-						height:2,
-						angle:0,
-						position:$h.Vector(mouse.x, mouse.y)
-					}
-					if($h.collides(mouse, that)){
-						that.click();
-					}
-				})
-			}
-		});
+		buttons();
 		$h.startGameButton.init();
 		$h.game = {};
 		$h.game.started = false;
@@ -89,6 +49,22 @@ define(["head-on", "player", "ship", "keys", "bullets"],function($h, player, shi
 		{
 			name:"dudeLeanLeft",
 			src:"img/dude_lean_left.png"
+		},
+		{
+			name:"dudeSitLeft",
+			src:"img/dude_sitting_left.png"
+		},
+		{
+			name:"dudeSitRight",
+			src:"img/dude_sitting_right.png"
+		},
+		{
+			name:"outOfFuelLeft",
+			src:"img/dude_front_left.png"
+		},
+		{
+			name:"outOfFuelRight",
+			src:"img/dude_front_right.png"
 		},
 		{
 			name:"ship",
--- js/jetpack.js
+++ js/jetpack.js
@@ -1,56 +1,71 @@
 define(["head-on"], function($h){
-	var fuel,
-		maxFuel,
-		fuelPerSecond,
-		refuelPerSecond,
-		refuelTimeout = 500,
-		lastTimeUp;
-	return{
-		useFuel:function(delta){
-			fuel -= fuelPerSecond * delta;
-			if(fuel < 0){
-				fuel = 0;
-			}
-			lastTimeUp = Date.now();
-		},
+	
+	return function(player){
+		var fuel,
+			maxFuel,
+			fuelPerSecond,
+			refuelPerSecond,
+			refuelTimeout = 3000,
+			lastTimeUp;
+		return{
+			useFuel:function(delta){
+				fuel -= fuelPerSecond * delta;
+				if(fuel < 0){
+					fuel = 0;
+				}
+				lastTimeUp = Date.now();
+				this.refuel = false;
+				this.lastTimeUp = Date.now();
+			},
 
-		setRefuelPerSecond: function(amt){
-			refuelPerSecond = amt;
-		},
+			setRefuelPerSecond: function(amt){
+				refuelPerSecond = amt;
+			},
 
-		setFuelPerSecond: function(amt){
-			fuelPerSecond = amt;
-		},
+			setFuelPerSecond: function(amt){
+				fuelPerSecond = amt;
+			},
 
-		setMaxFuel: function(amt){
-			fuel *= amt/maxFuel;
-			maxFuel = amt;
-		},
+			setMaxFuel: function(amt){
+				fuel *= amt/maxFuel;
+				maxFuel = amt;
+			},
 
-		setFuel: function(amt){
-			if(amt > maxFuel){
-				fuel = maxFuel;
-			}
-			else{
-				fuel = amt;
-			}
-		},
-
-		getMaxFuel: function(){
-			return maxFuel;
-		},
-
-		getFuel: function(){
-			return Math.floor(fuel);
-		},
-
-		update: function(delta){
-			if(Date.now()- lastTimeUp > refuelTimeout){
-				fuel += refuelPerSecond * delta;
-				if(fuel > maxFuel){
+			setFuel: function(amt){
+				if(amt > maxFuel){
 					fuel = maxFuel;
 				}
+				else{
+					fuel = amt;
+				}
+			},
+
+			getMaxFuel: function(){
+				return maxFuel;
+			},
+
+			getFuel: function(){
+				return Math.floor(fuel);
+			},
+
+			update: function(delta){
+				var that = this;
+				if(player.onGround || this.refuel){
+					fuel += refuelPerSecond * delta;
+					if(fuel > maxFuel){
+						fuel = maxFuel;
+					}
+				}
+				if(this.getFuel() <= 0){
+					if(Date.now() - lastTimeUp > refuelTimeout){
+						that.refuel = true;
+					}
+				}
+				// if(Date.now()- lastTimeUp > refuelTimeout){
+					
+				// }
 			}
 		}
 	}
+		
 });
